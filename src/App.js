5f057cf039cc59a8cd01f6c5c253cb9161dea88a
Alok <alok20530@iiitkalyani.ac.in>
2021-12-01 03:08:07 +0530

    deploying this app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  let pageSize = 9;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/"><News key="general" pagesize={pageSize} country="in" category="general" /></Route>
+          <Route exact path="/home"><News key="general" pagesize={pageSize} country="in" category="general" /></Route>
+          <Route exact path="/health"><News key="health" pagesize={pageSize} country="in" category="health" /></Route>
+          <Route exact path="/sports"><News key="sports" pagesize={pageSize} country="in" category="sports" /></Route>
+          <Route exact path="/science"><News key="science" pagesize={pageSize} country="in" category="science" /></Route>
+          <Route exact path="/business"><News key="business" pagesize={pageSize} country="in" category="business" /></Route>
+          <Route exact path="/technology"><News key="technology" pagesize={pageSize} country="in" category="technology" /></Route>
+          <Route exact path="/entertainment"><News key="entertainment" pagesize={pageSize} country="in" category="entertainment" /></Route>
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App;
